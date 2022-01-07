5c07472d916044eaee42dedd3e4321d917e58257
nongqi <2460822214@qq.com>
2022-01-07 11:06:49 +0800

    [提交]页面菜单的调整

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -1,16 +1,29 @@
+import menuListInfo from "../../config/menu.json"
+
 export default {
     name: 'index',
 
     data() {
-        return {}
+        return {
+            menuList: menuListInfo.menuInfo,
+            clickFlag: 0,
+        }
+    },
+
+    created() {
+
     },
 
     mounted() {
+
     },
 
     methods: {
         loginOut() {
             this.$router.replace('/login')
+        },
+        changeMenu(menuInfo, index) {
+            this.clickFlag = index
         }
     }
 }
--- src/view/index/index.vue
+++ src/view/index/index.vue
@@ -1,15 +1,38 @@
 <!--首页-->
 <template>
-    <el-container>
-        <el-header>
-            <el-button @click="loginOut">注销</el-button>
-        </el-header>
-        <el-container>
-            <el-aside width="200px">Aside</el-aside>
-            <el-container>
-                <el-main>Main</el-main>
-                <el-footer>Footer</el-footer>
-            </el-container>
+    <el-container class="home-page">
+        <el-aside width="350px" class="home-page-aside">
+            <div class="aside-menu-title">menu</div>
+            <div class="aside-menu">
+                <div class="aside-menu-li" v-for="(menu,index) in menuList" :key="menu.tag"
+                     @click="changeMenu(menu,index)">
+                    <div class="aside-menu-li-icon" :class="{'on-click-style':clickFlag === index}"><i
+                        class="aside-menu-icon" :class="menu.icon"></i></div>
+                    <span class="aside-menu-li-name"
+                          :class="{'on-click-font-style':clickFlag === index}">{{ menu.menuName }}</span>
+                </div>
+            </div>
+        </el-aside>
+        <el-container class="home-page-right-container">
+            <el-header class="home-page-header">
+                <div class="home-page-header-left"></div>
+                <div class="home-page-header-right">
+                    <el-avatar icon="el-icon-user-solid" size="small"></el-avatar>
+                    <el-dropdown trigger="click" class="dropdown">
+                        <span class="el-dropdown-link">
+                            nico<i class="el-icon-arrow-down el-icon--right"></i>
+                        </span>
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item>个人信息</el-dropdown-item>
+                            <el-dropdown-item>设置</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                    <el-tooltip class="item" effect="light" content="注销" placement="bottom">
+                        <i class="el-icon-switch-button login-out" @click="loginOut"></i>
+                    </el-tooltip>
+                </div>
+            </el-header>
+            <el-main>Main</el-main>
         </el-container>
     </el-container>
 </template>
